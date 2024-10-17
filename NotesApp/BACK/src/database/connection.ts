2dd88a0e64a7b9923feff3f1fffb9db8f3039b53
sezgox <128177757+sezgox@users.noreply.github.com>
2024-10-17 18:37:32 +0200

    Update connection.ts

--- NotesApp/BACK/src/database/connection.ts
+++ NotesApp/BACK/src/database/connection.ts
@@ -1,6 +1,6 @@
 import { Sequelize } from "sequelize";
 
-const sequelize = new Sequelize (process.env.SQL_SCHEMA ?? 'mynotes', 'root', process.env.SQL_PASSWORD ?? 'klkmanin2000', {
+const sequelize = new Sequelize (process.env.SQL_SCHEMA ?? 'mynotes', 'root', process.env.SQL_PASSWORD ?? 'db_pass', {
     host: 'localhost',
     dialect: 'mysql'
 });
