42d4e078b6e7e64d9d7fecfd98aeca8bff534c26
Sergio Elias <elias.szg2000@gmail.com>
2024-10-27 14:59:32 +0100

    projects added to portfolio

--- Portfolio/tailwind.config.mjs
+++ Portfolio/tailwind.config.mjs
@@ -2,7 +2,20 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				primary: {
+				  DEFAULT: '#01A592',
+				  light: '#56BABD'
+				},
+				secondary : {
+					DEFAULT: '#B2D1EB'
+				  }
+			  }
+			  
+		},
 	},
-	plugins: [],
+	plugins: [
+		
+	],
 }
