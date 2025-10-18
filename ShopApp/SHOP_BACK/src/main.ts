a0107e137572916e0b95bcc4bf854f9cd09dcd0e
Sergio <elias.szg2000@gmail.com>
2025-10-18 16:43:51 +0200

    meta info

--- ShopApp/SHOP_BACK/src/main.ts
+++ ShopApp/SHOP_BACK/src/main.ts
@@ -1,12 +1,19 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
-import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe({
-    whitelist: true
-  }));
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,           // 🚫 elimina propiedades no definidas en el DTO
+      forbidNonWhitelisted: true, // ❌ lanza error si llegan propiedades extra
+      transform: true,           // 🔄 transforma el body en una instancia del DTO
+      transformOptions: {
+        enableImplicitConversion: true, // ⚙️ convierte tipos automáticamente (string → number)
+      },
+    }),
+  );
   app.enableCors();
   await app.listen(3000);
 
